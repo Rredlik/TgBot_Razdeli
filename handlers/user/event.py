3389ec4d58f297c627d4ad183ea20a4daccb26bb
Rredlik <rredlik@mail.ru>
2024-01-22 13:58:15 +0500

    update

--- handlers/user/event.py
+++ handlers/user/event.py
@@ -6,7 +6,11 @@ from aiogram.dispatcher.filters import Text
 from aiogram.types import Message, CallbackQuery, ContentType
 
 from config import ADMIN_LINK
+<<<<<<< HEAD
+from database.methods.db_event import create_new_event, add_event_member, get_event_by_id
+=======
 from database.methods.db_event import create_new_event, add_event_member
+>>>>>>> 733588f7e4d4b7056534b357ff9d58ff5fae8575
 from database.methods.db_user import user_id_by_tg_id
 from filters.main import IsSubscriber
 from handlers.keyboards import btn_create_event
@@ -29,10 +33,20 @@ async def __event_created(msg: Message):
     await add_event_member(event_id, msg.from_user.id)
     await bot.send_message(chat_id=msg.from_user.id,
                            text=f'Мероприятие создано')
+    await __send_event(msg, event_id)
+
+async def __send_event(msg: Message, event_id=None):
+    event = await get_event_by_id(event_id)
+    event_name = event[1]
+    event_date = event[2]
+    event_message = (f'ID Меропириятия: {event_id}\n'
+                     f'Название: {event_name}\n'
+                     f'')
+    await bot.send_message(chat_id=msg.from_user.id,
+                           text=f'event_message')
 
 
     await bot.send_message()
-
 
 
 def register_event_handlers(dp: Dispatcher) -> None:
